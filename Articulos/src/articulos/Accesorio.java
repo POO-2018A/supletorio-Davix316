72ea4ba99ea4fca2fdf4cfadc7b31017de2cd633
David Curipoma <38595374+Davix316@users.noreply.github.com>
2018-08-16 16:00:15 -0500

    Add files via upload

---
+++ Articulos/src/articulos/Accesorio.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package articulos;
+
+/**
+ *
+ * @author ESFOT
+ */
+public class Accesorio {
+    
+    private String tipo;
+    private String color;
+
+    public Accesorio(String tipo, String color) {
+        this.tipo = tipo;
+        this.color = color;
+    }
+
+    public Accesorio() {}
+    
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+    
+    
+}
